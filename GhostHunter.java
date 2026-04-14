9c8dbac9f87c720a62aa4e31e3b9bac9268f83d5
Sonofwin1992 <aderkom1453@gmail.com>
2026-04-14 23:53:07 +0300

    .

---
+++ GhostHunter.java
@@ -0,0 +1,50 @@
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GhostHunter extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		Pane pane = new Pane();
+		pane.setPrefSize(800, 600);
+		
+		ButtonPane startButton = new ButtonPane("START GAME");
+		ButtonPane selectLevelButton = new ButtonPane("SELECT LEVEL");
+		ButtonPane exitButton = new ButtonPane("EXIT");
+		
+		pane.getChildren().add(exitButton);
+		pane.getChildren().add(selectLevelButton);
+		pane.getChildren().add(startButton);
+		
+		startButton.setLayoutX(100);
+		startButton.setLayoutY(400);
+		
+		selectLevelButton.setLayoutX(100);
+		selectLevelButton.setLayoutY(460);
+		
+		exitButton.setLayoutX(100);
+		exitButton.setLayoutY(520);
+		
+		// Image homeImage = new Image("");
+		// ImageView homeImageView = new ImageView(homeImage);
+		
+		// Image iconImage = new Image("");
+		//İcon unutma!!! stage.getIcons().add(iconImage);
+		
+		Scene homeScene = new Scene(pane);
+		stage.setScene(homeScene);
+		stage.setTitle("GHOST HUNTER");
+		stage.show();
+		
+	}
+
+}
