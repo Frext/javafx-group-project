3f0bc2b793caa1f0d1134fadbf07c960ff87e062
Furkan Usul <furkanusul28@gmail.com>
2026-04-15 20:08:59 +0300

    Organize source files

--- java/ButtonPane.java
+++ java/ButtonPane.java
@@ -1,3 +1,4 @@
+package com.group1.groupproject;
 
 import javafx.geometry.Pos;
 import javafx.scene.control.Label;
--- java/GhostHunter.java
+++ java/GhostHunter.java
@@ -1,6 +1,6 @@
+package com.group1.groupproject;
+
 import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 
 import javafx.application.Application;
