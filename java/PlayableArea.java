de266287d31753b34498e82ded3dfa226298f235
Emir0903 <emir98241@gmail.com>
2026-05-14 19:55:43 +0300

    Update PlayableArea.java

--- java/PlayableArea.java
+++ java/PlayableArea.java
@@ -4,7 +4,8 @@ import javafx.scene.layout.Pane;
 public class PlayableArea extends Pane{
 	private double minX , minY , maxX , maxY;
 	private double width , height;
-	// We'll create panes for playAreas for each level
+	
+	//this is where we add entities and token Emir Aydın 150124001
 	public PlayableArea(double x , double y , double width, double height) {
 		minX = x;
 		minY = y;
@@ -18,7 +19,8 @@ public class PlayableArea extends Pane{
 		playableArea.setLayoutX(minX);
 		playableArea.setLayoutY(minY);
 		playableArea.setFocusTraversable(true); // This is keyboard listener
-		// Abdullah Derviş Kombıçak 150124009 M button action.
+		
+		// Abdullah Derviş Kombıçak 150124009 M button action which makes visible the playable area border and enemies.
 		playableArea.setOnKeyPressed(e -> {
 		if (e.getCode() == KeyCode.M) {
 			playableArea.setStyle("-fx-border-color: red; -fx-border-width: 1px;");
